052a17ac765a44e5431efc951f6aeee4718da84c
Victor Fonseca <victorlfonseca@gmail.com>
2022-02-20 22:27:42 -0300

    Adding Poppins font-family.

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -4,15 +4,27 @@ import 'screens/home_screen.dart';
 class AppWidget extends StatelessWidget {
   const AppWidget({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primaryColor: const Color(0xFF055AA3),
+        highlightColor: const Color(0xFFEDF4F8),
+        backgroundColor: const Color(0xFF121517),
+        scaffoldBackgroundColor: const Color(0xFF121517),
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Color(0xFF121517),
+        ),
+        fontFamily: 'Poppins',
+        textTheme: const TextTheme(
+          headline1: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
       ),
-      home: const HomeScreen(title: 'Flutter Demo Home Page'),
+      home: const HomeScreen(title: 'Dark Demo Home Page'),
     );
   }
 }
