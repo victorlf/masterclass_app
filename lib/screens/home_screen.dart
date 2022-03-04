62393533749bff0f3bc0162b3514f8c063c089df
Victor Fonseca <victorlfonseca@gmail.com>
2022-03-04 12:21:14 -0300

    fix: disable pageview horizontal scroll

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -78,6 +78,7 @@ class _HomeScreenState extends State<HomeScreen> {
       child: Scaffold(
         appBar: CustomAppBar(labels[selectedIndex].keys.first),
         body: PageView.builder(
+          physics: const NeverScrollableScrollPhysics(),
           controller: controller,
           itemCount: children.length,
           itemBuilder: (context, index) {
